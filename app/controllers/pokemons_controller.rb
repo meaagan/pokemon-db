efebc159cfd22c3a68127f88feaf36725e5a6c28
Meagan Butters <meagan.butters@hotmail.com>
2020-12-07 14:51:20 -0500

    Added more routes and more controller actions, added information to show

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -6,4 +6,33 @@ class PokemonsController < ApplicationController
     def show
         @pokemon = Pokemon.find(params[:id])
     end
+
+    def create 
+        @pokemon = Pokemon.new(pokemon_params)
+        
+        if @pokemon.save!
+            redirect_to pokemon_path(@pokemon)
+        else
+            render :new
+        end
+    end
+
+    def new
+        @pokemon = Pokemon.new
+    end
+
+    def edit
+        @pokemon = Pokemon.find(params[:id])
+    end
+
+    def update 
+        @pokemon = Pokemon.find(params[:id])
+    end
+
+
+    private
+
+    def pokemon_params
+        params.require(:pokemon).permit(:number, :name, :type_1, :type_2, :total, :hp, :attack, :defense, :sp_atk, :sp_def, :speed, :generation, :legendary)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :pokemons, only: [:index, :show]
+  resources :pokemons
 end
